b2ceb75609178d4591bac782e92d6fc467c6f7ce
Cole Roof <ckroof@gmail.com>
2019-04-04 18:30:40 -0400

    First commit with matrix design~

--- code/main.py
+++ code/main.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
+from pad4pi import rpi_gpio as KEY
 import time
 import subprocess
 
 # Global Vars
-last_button_pressed = -1
+last_button_pressed = "None"
 switch_state = -1
 
-# Dictionary of all of the media filenames or fodler names
+# Dictionary of all of the media filenames or folder names
 media = {
     "outreach": ["Outreach 2019 V7 Renammed", "outreach.mp4"],
     "build": ["build", "build"],
@@ -28,51 +29,40 @@ media = {
     "team_history": ["history", "history"]
 }
 
-# Mapping of physical pin number to its media name
-pin_vals = {
-    3: "outreach",
-    5: "build",
-    7: "graphics",
-    8: "website",
-    10: "electronics",
-    11: "programming",
-    12: "playbook",
-    15: "3d_print",
-    16: "steam_expo",
-    18: "summer_camp_vid",
-    23: "dt_intake",
-    24: "vision_pathfollowing",
-    26: "bellypan_pneumatics",
-    29: "elevator",
-    31: "climber",
-    32: "flower_cargoshooter",
-    33: "hosting_comps",
-    35: "team_history",
-}
+# These are the values that will output to the callback function whenever the buttons are pressed
+keypad_vals = [
+    ["outreach", "build", "3d_print", "programming"],
+    ["graphics", "website", "playbook", "electronics"],
+    ["steam_expo", "summer_camp_vid", "left", "right"],
+    ["vision_pathfollowing", "bellypan_pneumatics", "hosting_comps", "team_history"],
+    ["dt_intake", "elevator", "climber", "flower_cargoshooter"]
+]
+# There are 4
+# The library sets the columns as outputs
+# I am following the schematic of Mr. Luban's for reference
+keypad_cols_pins = [36, 37, 38, 40]
+# There are 5
+# The library sets the rows as inputs
+# I am following the schematic of Mr. Luban's for reference
+keypad_rows_pins = [5, 7, 11, 13, 15]
+
+# An object the library needs to create other keypads
+keypad_creator = KEY.KeypadFactory()
 
 # These are functional pins that don't have media attached to them
-left_pin = 19
-right_pin = 21
-end_program = 22
-switch_read_pin = 36
 
 # Time in ms
 pin_debounce_time = 42
-# Time in s
-# Time between when different buttons can be pressed
-multi_button_delay = 5
-
 
 # Functions
+def my_callback(pin_name):
+    global last_button_pressed, switch_read_pin, media
 
-def my_callback(channel):
-    global last_button_pressed, switch_read_pin, pin_vals, media
+    if last_button_pressed != pin_name:
 
-    if last_button_pressed != channel:
-
-        if channel == left_pin:
+        if pin_name == "left":
             subprocess.call("xdotool key Left")
-        elif channel == right_pin:
+        elif pin_name == "right":
             subprocess.call("xdotool key Right")
         else:
 
@@ -81,55 +71,22 @@ def my_callback(channel):
             switch_state = GPIO.input(switch_read_pin)
 
             # Basically cross reference the pin number with the media file names
-            file_name = media[pin_vals[channel]][switch_state]
+            file_name = media[pin_name][switch_state]
             subprocess.call("./switch_process.sh ../media/" +
                             file_name, shell=True)
 
             # Makes itso you cant mash the same button while its already running
-            last_button_pressed = channel
-
-            # Wait for a while so the buttons can't be mashed
-            time.sleep(multi_button_delay)
+            last_button_pressed = pin_name
 
 
+# This creates the keypad and then sets up the switch read and end program
 def setup_pins():
-    global pin_vals, pin_debounce_time, left_pin, right_pin, end_program
-    global switch_read_pin
+    # Creating the actual keypad
+    keypad = keypad_creator.create_keypad(
+        keypad=keypad_vals, row_pins=keypad_rows_pins, col_pins=keypad_cols_pins, gpio_mode=GPIO.BOARD)
 
-    # Set up the GPIO pin numbering system
-    # BOARD is the position on the board
-    # BCM is the numbering of the GPIO
-    GPIO.setmode(GPIO.BOARD)
-
-    # Loop through all of the values in the pins dictionary
-    # Set them up as inputs
-    # Add the callback to the pins to trigger an event
-    for pin_num in pin_vals.keys():
-        GPIO.setup(pin_num, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(pin_num, GPIO.RISING, callback=my_callback,
-                              bouncetime=pin_debounce_time)
-        print("Setup " + pin_vals[pin_num] + " pin")
-    # Now setup all of the control pins (l,r,end,switch)
-    GPIO.setup(left_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(left_pin, GPIO.RISING,
-                          callback=my_callback, bouncetime=pin_debounce_time)
-    GPIO.setup(right_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(right_pin, GPIO.RISING,
-                          callback=my_callback, bouncetime=pin_debounce_time)
-
-    # The switch that tells which media to use
-    # No callback needed, just a passive read pin
-    GPIO.setup(switch_read_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    # End Program pin
-    # No callback required
-    GPIO.setup(end_program, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    print('Pin setup complete')
-    # Pause and let the computer catch up
-    # Number in seconds
-    time.sleep(0.5)
-
+    # Apply the callback function to whenever any of the keys are pressed
+    keypad.registerKeyPressHandler(my_callback)
 
 # Main
 # This program works on callback functions whenever a button is pressed
